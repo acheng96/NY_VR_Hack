5613089aa17043ee5b70e10d5290594cb5d4ce3b
liyiming-serif <liyiming.serif@gmail.com>
2017-02-18 16:26:58 -0500

    added side script

--- Assets/Scripts/SideControlled.cs
+++ Assets/Scripts/SideControlled.cs
@@ -9,23 +9,43 @@ public class SideControlled : MonoBehaviour {
 
 	public static SideControlled singleton;
 
+    [SerializeField]
+    private GameObject colorMe;
+    [SerializeField]
+    private GameObject lerpMe;
+
+    private Renderer rend;
+    private Renderer lerp;
+
 	// Use this for initialization
 	void Awake () {
 		singleton = this;
+        rend = colorMe.GetComponent<Renderer>();
+        lerp = lerpMe.GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame. Used for button inputs
 	void Update () {
+        if(Input.GetButtonDown("Oculus_GearVR_LIndexTrigger") || Input.GetButtonDown("Oculus_GearVR_RIndexTrigger")){
+            rend.material.SetColor("_Color", Color.red);
+        }
         if(Input.GetButtonUp("Oculus_GearVR_LIndexTrigger") || Input.GetButtonUp("Oculus_GearVR_RIndexTrigger")){
-			Debug.Log("button pressed!");
+            rend.material.SetColor("_Color", Color.white);
 		}
+
+        if(Input.GetButtonDown("Fire1")){
+            rend.material.SetColor("_Color", Color.red);
+        }
+        if(Input.GetButtonUp("Fire1")){
+            rend.material.SetColor("_Color", Color.white);
+        }
+
 	}
 
 	//Used for swiping actions.
 	void FixedUpdate () {
-		float a = Input.GetAxis("Oculus_GearVR_DpadY");
-		if (Mathf.Abs(a) > 0.005){
-			Debug.Log(a);
-		}
+        //float a = (Input.GetAxis("Oculus_GearVR_DpadY")+1)/2;
+        float a = (Input.GetAxis("vertical")+1)/2;
+        lerp.material.SetColor("_Color", Color.Lerp(Color.white, Color.green, a));
 	}
 }
