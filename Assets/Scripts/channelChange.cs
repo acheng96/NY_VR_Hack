205f1513d4f6ad93aa6eba64b50569110d2aad52
Chang <cl795@cornell.edu>
2017-02-18 21:33:53 -0500

    Add channel surfing

--- Assets/Scripts/channelChange.cs
+++ Assets/Scripts/channelChange.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class channelChange : MonoBehaviour {
 
     public GameObject sphere;
-    Material[] mats;
     int counter = 0;
+    public Texture t0;
+    public Texture t1;
+    public Texture t2;
+    public Texture t3;
+    public Texture t4;
+    public Texture t5;
+    public Texture t6;
+    Texture[] textures;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         // Create a material with transparent diffuse shader
-        mats = new Material[]{ new Material(Shader.Find("picnic")), new Material(Shader.Find("concert"))};
+        textures = new Texture[] { t0,t1,t2,t3,t4,t5,t6 };
     }
 	
 	// Update is called once per frame
 	void Update () {
         float a = Input.GetAxis("Mouse X");
-        if (a > 5)
+        if (a > 10)
         {
             counter++;
-            counter = counter % 2;
+            counter = counter % textures.Length;
         }
-
-        sphere.GetComponent<Renderer>().material = mats[counter];
-    }
+        sphere.GetComponent<Renderer>().material.mainTexture = textures[counter];
+        }
 }
